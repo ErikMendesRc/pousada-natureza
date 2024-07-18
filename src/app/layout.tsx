bab1e6abe4a742c2b248ff16ad64747674140cd9
ErikMendesRC <erik.mendes19@gmail.com>
2024-07-18 02:34:45 -0300

    Ajustado SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,31 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Pousada Natureza - Pesca Esportiva na Amazônia",
   description: "Pousada Natureza - Pesca esportiva no Rio Preto da Eva, Amazônia. Desfrute de uma experiência única de pesca do tucunaré açu com conforto e comodidade.",
+  keywords: "pesca esportiva, tucunaré açu, Rio Preto da Eva, Amazônia, lodge de pesca, pesca no Amazonas",
+  openGraph: {
+    title: "Pousada Natureza - Pesca Esportiva na Amazônia",
+    description: "Localizada no coração da Amazônia, no Rio Preto da Eva, a Pousada Natureza é o destino perfeito para os amantes da pesca esportiva.",
+    url: "https://www.pousadanaturezaeva.com.br",
+    type: "website",
+    images: [
+      {
+        url: "https://www.pousadanaturezaeva.com.br/images/logo.png",
+        width: 800,
+        height: 600,
+        alt: "Pousada Natureza Logo"
+      }
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@pousadanaturezaeva",
+    title: "Pousada Natureza - Pesca Esportiva na Amazônia",
+    description: "Localizada no coração da Amazônia, no Rio Preto da Eva, a Pousada Natureza é o destino perfeito para os amantes da pesca esportiva.",
+    images: ["https://www.pousadanaturezaeva.com.br/images/logo.png"]
+  },
+  alternates: {
+    canonical: "https://www.pousadanaturezaeva.com.br"
+  }
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import HeroSection from "../components/HeroSection";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
@@ -9,14 +10,47 @@ import LocationSection from "@/components/LocationSection";
 
 export default function Home() {
   return (
-    <main>
-      <HeroSection />
-      <AboutSection />
-      <ServicesSection />
-      <TestimonialsSection />
-      <GallerySection />
-      <ReservationSection />
-      <LocationSection/>
-    </main>
+    <>
+      <Head>
+        <title>Pousada Natureza - Pesca Esportiva na Amazônia</title>
+        <meta name="description" content="Localizada no coração da Amazônia, no Rio Preto da Eva, a Pousada Natureza é o destino perfeito para os amantes da pesca esportiva. Especializamos na pesca do tucunaré açu, oferecendo uma experiência inesquecível em meio à exuberante natureza amazônica." />
+        <meta name="keywords" content="pesca esportiva, tucunaré açu, Rio Preto da Eva, Amazônia, lodge de pesca, pesca no Amazonas" />
+        <meta property="og:title" content="Pousada Natureza - Pesca Esportiva na Amazônia" />
+        <meta property="og:description" content="Localizada no coração da Amazônia, no Rio Preto da Eva, a Pousada Natureza é o destino perfeito para os amantes da pesca esportiva." />
+        <meta property="og:image" content="https://www.pousadanaturezaeva.com.br/images/logo.png" />
+        <meta property="og:url" content="https://www.pousadanaturezaeva.com.br" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@pousadanaturezaeva" />
+        <meta name="twitter:title" content="Pousada Natureza - Pesca Esportiva na Amazônia" />
+        <meta name="twitter:description" content="Localizada no coração da Amazônia, no Rio Preto da Eva, a Pousada Natureza é o destino perfeito para os amantes da pesca esportiva." />
+        <meta name="twitter:image" content="https://www.pousadanaturezaeva.com.br/images/logo.png" />
+        <link rel="canonical" href="https://www.pousadanaturezaeva.com.br" />
+      </Head>
+      <main>
+        <header>
+          <HeroSection />
+        </header>
+        <section id="about">
+          <AboutSection />
+        </section>
+        <section id="services">
+          <ServicesSection />
+        </section>
+        <section id="testimonials">
+          <TestimonialsSection />
+        </section>
+        <section id="gallery">
+          <GallerySection />
+        </section>
+        <section id="reservation">
+          <ReservationSection />
+        </section>
+        <section id="location">
+          <LocationSection />
+        </section>
+        <footer>
+        </footer>
+      </main>
+    </>
   );
 }
